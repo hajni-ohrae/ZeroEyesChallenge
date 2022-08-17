442f124c7ef343831fd24bdd03fc9378ded49dc1
kbkim-ohrae <79883789+kyungbonkim@users.noreply.github.com>
2022-08-17 15:44:07 +0900

    Add(RewardHistoryItem)

---
+++ challenge/src/main/java/biz/ohrae/challenge/ui/components/list_item/RewardHistoryItem.kt
@@ -0,0 +1,136 @@
+package biz.ohrae.challenge.ui.components.list_item
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import biz.ohrae.challenge.ui.theme.TextBlack
+import biz.ohrae.challenge.ui.theme.dpToSp
+import biz.ohrae.challenge.ui.theme.myTypography
+
+@Preview(
+    showBackground = true,
+    widthDp = 360
+)
+@Composable
+private fun RewardHistoryItemGallery() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp, 0.dp)
+    ) {
+        RewardHistoryItem(
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = 105.dp),
+            date = "2022.04.25  09:33",
+            progress = "100%",
+            title = "미라클 모닝, 일찍 일어나기",
+            price = "10,000원",
+            state = "2022.04.14 소멸",
+        )
+        Divider(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Color(0xfffafafa)))
+        RewardHistoryItem(
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = 105.dp),
+            date = "2022.04.25  09:33",
+            progress = "85%",
+            title = "달리기 챌린지",
+            price = "1,000원",
+            state = "2022.04.14 소멸",
+        )
+        Divider(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Color(0xfffafafa)))
+        RewardHistoryItem(
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = 105.dp),
+            date = "2022.04.25  09:33",
+            progress = "30%",
+            title = "책읽기 챌린지",
+            price = "10,000원",
+        )
+    }
+}
+
+@Composable
+fun RewardHistoryItem(
+    modifier: Modifier = Modifier,
+    date: String,
+    progress: String,
+    title: String,
+    price: String,
+    state: String? = null,
+    isCanceled: Boolean = false,
+) {
+    val priceColor = if (isCanceled) {
+        remember { mutableStateOf(Color(0xff6c6c6c)) }
+    } else {
+        remember { mutableStateOf(Color(0xff4985f8)) }
+    }
+
+    Column(
+        modifier = modifier.padding(0.dp, 18.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = date,
+                style = myTypography.default,
+                color = Color(0xff6c6c6c),
+                fontSize = dpToSp(dp = 14.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = progress,
+                style = myTypography.default,
+                color = Color(0xff6c6c6c),
+                fontSize = dpToSp(dp = 14.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                style = myTypography.bold,
+                color = TextBlack,
+                fontSize = dpToSp(dp = 16.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = price,
+                style = myTypography.bold,
+                color = priceColor.value,
+                fontSize = dpToSp(dp = 16.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        if (state != null) {
+            Text(
+                text = state,
+                style = myTypography.default,
+                color = Color(0xff6c6c6c),
+                fontSize = dpToSp(dp = 14.dp)
+            )
+        }
+    }
+}
