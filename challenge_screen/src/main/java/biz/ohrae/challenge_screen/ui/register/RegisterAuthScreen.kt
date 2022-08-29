e1c06649ed0a122f5e2d83792374299b02890e0d
hajni-ohrae <hajnii@ohrae.biz>
2022-08-29 14:09:59 +0900

    Add(RegisterActivity)

---
+++ challenge_screen/src/main/java/biz/ohrae/challenge_screen/ui/register/RegisterAuthScreen.kt
@@ -0,0 +1,17 @@
+package biz.ohrae.challenge_screen.ui.register
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import biz.ohrae.challenge.model.MainScreenState
+
+
+@Preview(
+    widthDp = 360,
+    heightDp = 720,
+    showBackground = true
+)
+@Composable
+fun RegisterAuthScreen(
+) {
+
+}
