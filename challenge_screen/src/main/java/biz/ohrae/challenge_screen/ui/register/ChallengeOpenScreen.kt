2080578159fe5d9475072949840342ebbc92b106
hajni-ohrae <hajnii@ohrae.biz>
2022-08-30 09:56:56 +0900

    Fix(ChallengeOpenScreen)

--- challenge_screen/src/main/java/biz/ohrae/challenge_screen/ui/register/ChallengeOpenScreen.kt
+++ challenge_screen/src/main/java/biz/ohrae/challenge_screen/ui/register/ChallengeOpenScreen.kt
@@ -2,7 +2,6 @@ package biz.ohrae.challenge_screen.ui.register
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import biz.ohrae.challenge.model.MainScreenState
 
 
 @Preview(
