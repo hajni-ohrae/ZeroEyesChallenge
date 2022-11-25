20f4603cc7435de9c2c150d1ac4888ad8a9302a3
kbkim-ohrae <79883789+kyungbonkim@users.noreply.github.com>
2022-11-25 14:13:32 +0900

    Fix(keyboard behavior)

--- challenge_screen/src/main/java/biz/ohrae/challenge_screen/ui/profile/ChallengeInitProfileScreen.kt
+++ challenge_screen/src/main/java/biz/ohrae/challenge_screen/ui/profile/ChallengeInitProfileScreen.kt
@@ -36,8 +36,42 @@ fun ChallengeInitProfileScreen(
     val scrollState = rememberScrollState()
     Column(
          modifier = Modifier
-             .fillMaxSize()
-             .verticalScroll(scrollState)
+             .fillMaxWidth()
+             .fillMaxHeight()
+             .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        var nickname by remember { mutableStateOf("") }
+
+        TopArea(
+            user = user,
+            profileImageUri = profileImageUri,
+            nicknameState = nicknameState,
+            clickListener = clickListener,
+            onValueChange = {
+                nickname = it
+            },
+            nickname = nickname
+        )
+        BottomArea(
+            nicknameState = nicknameState,
+            clickListener = clickListener,
+            nickname = nickname
+        )
+    }
+}
+
+@Composable
+private fun TopArea(
+    user: User = User.mock(),
+    profileImageUri: Uri? = null,
+    nicknameState: NicknameState? = null,
+    clickListener: ChallengeProfileClickListener? = null,
+    nickname: String,
+    onValueChange: (String) -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(24.dp, 0.dp)
     ) {
         UploadProfileImage(
             profileImageUri = profileImageUri,
@@ -46,72 +80,79 @@ fun ChallengeInitProfileScreen(
             }
         )
         Spacer(modifier = Modifier.height(20.dp))
-        Column(
+        val scope = rememberCoroutineScope()
+        val focusRequester = remember { FocusRequester() }
+
+        scope.launch {
+            delay(100)
+            focusRequester.requestFocus()
+        }
+
+        Text(
+            text = "닉네임*",
+            fontSize = dpToSp(dp = 16.dp),
+            color = TextBlack,
+            style = myTypography.extraBold
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextBoxWithButton(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f)
-                .padding(24.dp, 0.dp)
-        ) {
-            val scope = rememberCoroutineScope()
-            var nickname by remember { mutableStateOf("") }
-            val focusRequester = remember { FocusRequester() }
-
-            scope.launch {
-                delay(100)
-                focusRequester.requestFocus()
+                .aspectRatio(7.09f)
+                .focusRequester(focusRequester),
+            placeholder = "닉네임(10자리)",
+            maxLength = 10,
+            value = nickname,
+            onValueChange = {
+                onValueChange(it)
+            },
+            buttonName = "중복확인",
+            singleLine = true,
+            onClickButton = {
+                clickListener?.onClickCheckNickname(nickname)
             }
-
-            Text(
-                text = "닉네임*",
-                fontSize = dpToSp(dp = 16.dp),
-                color = TextBlack,
-                style = myTypography.extraBold
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            TextBoxWithButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(7.09f)
-                    .focusRequester(focusRequester),
-                placeholder = "닉네임(10자리)",
-                maxLength = 10,
-                value = nickname,
-                onValueChange = {
-                    nickname = it
-                },
-                buttonName = "중복확인",
-                singleLine = true,
-                onClickButton = {
-                    clickListener?.onClickCheckNickname(nickname)
-                }
-            )
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "닉네임은 한글, 영문, 숫자, _ . - 만 입력 가능",
+            fontSize = dpToSp(dp = 12.dp),
+            color = Color(0xff707070),
+        )
+        if (nicknameState != null) {
             Spacer(modifier = Modifier.height(10.dp))
             Text(
-                text = "닉네임은 한글, 영문, 숫자, _ . - 만 입력 가능",
+                text = nicknameState.message,
                 fontSize = dpToSp(dp = 12.dp),
-                color = Color(0xff707070),
+                color = if (nicknameState.success) Color(0xff36ad3b) else Color.Red,
             )
-            if (nicknameState != null) {
-                Spacer(modifier = Modifier.height(10.dp))
-                Text(
-                    text = nicknameState.message,
-                    fontSize = dpToSp(dp = 12.dp),
-                    color = if (nicknameState.success) Color(0xff36ad3b) else Color.Red,
-                )
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            FlatButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(6.5f),
-                text = "완료",
-                backgroundColor = Color(0xff003865),
-                enabled = nicknameState?.success == true,
-                onClick = {
-                    clickListener?.onClickChangeNickname(nickname)
-                }
-            )
-            Spacer(modifier = Modifier.height(24.dp))
         }
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+}
+
+@Composable
+private fun BottomArea(
+    nicknameState: NicknameState? = null,
+    clickListener: ChallengeProfileClickListener? = null,
+    nickname: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp, 0.dp)
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+        FlatButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(6.5f),
+            text = "완료",
+            backgroundColor = Color(0xff003865),
+            enabled = nicknameState?.success == true,
+            onClick = {
+                clickListener?.onClickChangeNickname(nickname)
+            }
+        )
+        Spacer(modifier = Modifier.height(24.dp))
     }
 }
