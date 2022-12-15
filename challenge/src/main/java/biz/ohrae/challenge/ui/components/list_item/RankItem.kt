6d532fde0d5aa1bc8f947a15885005da632e3d41
hajni-ohrae <hajnii@ohrae.biz>
2022-12-15 15:05:31 +0900

    Fix(Layout)

--- challenge/src/main/java/biz/ohrae/challenge/ui/components/list_item/RankItem.kt
+++ challenge/src/main/java/biz/ohrae/challenge/ui/components/list_item/RankItem.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import biz.ohrae.challenge.model.list_item.RankItemData
@@ -24,17 +26,18 @@ import biz.ohrae.challenge.ui.theme.myTypography
 @Composable
 fun RankItemGallery() {
     val list = listOf(
-        RankItemData("", "1","이오래", "30시간 10분", "100%"),
-        RankItemData("", "2","김오래", "30시간 10분", "100%"),
-        RankItemData("", "3","최오래", "30시간 10분", "100%"),
-        RankItemData("", "4","박오래", "30시간 10분", "100%"),
+        RankItemData("", "1", "이오래", "30시간 10분", "100%"),
+        RankItemData("", "2", "김오래", "10회", "100%"),
+        RankItemData("", "3", "최오래dededededede", "30시간 10분", "100%"),
+        RankItemData("", "4", "박오래", "30시간 10분", "100%"),
     )
 
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
     ) {
         items(list) { rank ->
-            RankItem(rank.rank, rank.userName, rank.timeDays, rank.progress, "10회", isMe = true)
+            RankItem(rank.rank, rank.userName, rank.timeDays, rank.progress, "", isMe = true)
+//            Spacer(modifier = Modifier.width(16.dp))
         }
     }
 }
@@ -60,9 +63,7 @@ fun RankItem(
     }
 
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(36.dp),
+        modifier = Modifier.fillMaxWidth().height(36.dp),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -74,81 +75,99 @@ fun RankItem(
                     AvatarWithNumber(
                         modifier = Modifier
                             .fillMaxHeight()
+                            .weight(0.115F)
                             .aspectRatio(1f),
                         number = rank,
                         url = profileImage.toString(),
                     )
+                    Spacer(modifier = Modifier.weight(0.016F))
                 }
+
                 "2" -> {
                     AvatarWithNumber(
                         modifier = Modifier
                             .fillMaxHeight()
+                            .weight(0.115F)
                             .aspectRatio(1f),
                         number = rank,
                         url = profileImage.toString(),
                         borderColor = Color(0xff6fcf97),
                         numberBackgroundColor = Color(0xff1aaf5a)
                     )
+                    Spacer(modifier = Modifier.weight(0.016F))
+
                 }
                 "3" -> {
                     AvatarWithNumber(
                         modifier = Modifier
                             .fillMaxHeight()
+                            .weight(0.115F)
                             .aspectRatio(1f),
                         number = rank,
                         url = profileImage.toString(),
                         borderColor = Color(0xfff2994a),
                         numberBackgroundColor = Color(0xffeb5757)
                     )
+                    Spacer(modifier = Modifier.weight(0.016F))
                 }
                 else -> {
                     circularAvatar(
                         modifier = Modifier
                             .fillMaxHeight()
+                            .weight(0.115F)
                             .aspectRatio(1f),
                         url = profileImage.toString()
                     )
+                    Spacer(modifier = Modifier.weight(0.016F))
                 }
             }
-            Spacer(modifier = Modifier.width(16.dp))
             Text(
+                modifier = Modifier.weight(0.096F),
                 text = rank,
                 style = myTypography.extraBold,
                 fontSize = dpToSp(dp = 14.dp),
-                color = TextBlack
+                color = TextBlack,
+                textAlign = TextAlign.Center
             )
-            Spacer(modifier = Modifier.width(16.dp))
             Text(
+                modifier = Modifier.weight(0.314F),
                 text = name,
                 style = myTypography.default,
                 fontSize = dpToSp(dp = 14.dp),
-                color = TextBlack
+                color = TextBlack,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Left
             )
-            Spacer(modifier = Modifier.weight(1f))
             if (timeDays.isNotEmpty()) {
                 Text(
+                    modifier = Modifier.weight(0.314F),
                     text = timeDays,
-                    style = myTypography.extraBold,
+                    style = myTypography.bold,
                     fontSize = dpToSp(dp = 14.dp),
-                    color = TextBlack
+                    color = TextBlack,
+                    textAlign = TextAlign.Right
                 )
-                Spacer(modifier = Modifier.width(12.dp))
             }
             if (count.isNotEmpty()) {
                 Text(
                     text = count,
                     style = myTypography.bold,
                     fontSize = dpToSp(dp = 14.dp),
-                    color = TextBlack
+                    color = TextBlack,
+                    textAlign = TextAlign.Right
                 )
-                Spacer(modifier = Modifier.width(12.dp))
+                Spacer(modifier = Modifier.weight(0.038F))
             }
+            Spacer(modifier = Modifier.weight(0.016F))
             if (progress.isNotEmpty()) {
                 Text(
+                    modifier = Modifier.weight(0.144F),
                     text = progress,
                     style = myTypography.bold,
                     fontSize = dpToSp(dp = 14.dp),
-                    color = Color(0xff005bad)
+                    color = Color(0xff005bad),
+                    textAlign = TextAlign.Right
                 )
             }
         }
